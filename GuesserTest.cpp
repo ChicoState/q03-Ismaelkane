659d82b13843180465200e634d870f300253ddef
Ismael Kane <ismael9kane@gmail.com>
2025-05-05 01:46:15 -0700

    added comments to tests

--- GuesserTest.cpp
+++ GuesserTest.cpp
@@ -44,25 +44,25 @@ TEST(GuesserTest, three_remaining)
   Guesser object("Secret");
   bool actual = object.match("Secret");
    unsigned int remaining = object.remaining();
-   ASSERT_EQ( remaining, 3 );
+   ASSERT_EQ( remaining, 3 ); // should be 3 because guess is correct
 }
 
 TEST(GuesserTest, two_remaining)
 {
   Guesser object("Secret");
-  bool actual = object.match("Secre");
+  bool actual = object.match("Secre"); //m_remaing decreased by 1
    unsigned int remaining = object.remaining();
-   ASSERT_EQ( remaining, 2 );
+   ASSERT_EQ( remaining, 2 ); //3-1=2
 }
 
 
 TEST(GuesserTest, one_remaining)
 {
   Guesser object("Secret");
-  bool actual = object.match("Secre");
-  bool actual2 = object.match("Secre");
+  bool actual = object.match("Secre"); //m_remaing decreased by 1
+  bool actual2 = object.match("Secre");//m_remaing decreased by 1
    unsigned int remaining = object.remaining();
-   ASSERT_EQ( remaining, 1 );
+   ASSERT_EQ( remaining, 1 ); //3-2=1
   
 }
 
@@ -70,37 +70,37 @@ TEST(GuesserTest, one_remaining)
 TEST(GuesserTest, zero_remaining)
 {
   Guesser object("Secret");
-  bool actual = object.match("Secre");
-  bool actual2 = object.match("Secre");
-  bool actual3 = object.match("Secre");
+  bool actual = object.match("Secre"); //m_remaing decreased by 1
+  bool actual2 = object.match("Secre"); //m_remaing decreased by 1
+  bool actual3 = object.match("Secre");//m_remaing decreased by 1
    unsigned int remaining = object.remaining();
-   ASSERT_EQ( remaining, 0 );
+   ASSERT_EQ( remaining, 0 ); //3-3=0
 }
 
 TEST(GuesserTest, locked_from_distance_greater_than_2)
 {
   Guesser object("Secret");
-  bool incorrect = object.match("Sec");
-   bool correct = object.match("Secret");
-    ASSERT_FALSE(correct);
+  bool incorrect = object.match("Sec"); //distance = 3 
+   bool correct_guess = object.match("Secret");
+    ASSERT_FALSE(correct_guess);//shoudld return false because distance is greater than 2 before correct guess is made
 }
 
 
 TEST(GuesserTest, not_locked_from_distance_of_2)
 {
   Guesser object("Secret");
-  bool incorrect = object.match("Secr");
-   bool correct = object.match("Secret");
-    ASSERT_TRUE(correct);
+  bool incorrect = object.match("Secr");  //distance = 2
+   bool correct_guess = object.match("Secret");
+    ASSERT_TRUE(correct_guess);// should be true because distance is not greater than 2 allowing to check password, which is the correct guess
   
 }
 
 TEST(GuesserTest, not_locked_from_distance_of_1)
 {
   Guesser object("Secret");
-  bool incorrect = object.match("Secre");
+  bool incorrect = object.match("Secre"); //distance =1
    bool correct = object.match("Secret");
-    ASSERT_TRUE(correct);
+    ASSERT_TRUE(correct);// should be true because distance is not greater than 2 allowing to check password, which is the correct guess
   
 }
 
@@ -109,12 +109,11 @@ TEST(GuesserTest, not_locked_from_distance_of_1)
 TEST(GuesserTest, locked_from_no_remaining_guesses)
 {
   Guesser object("Secret");
-  bool guess1 = object.match("Secre");
-  bool guess2 = object.match("Secre");
-  bool guess3 = object.match("Secre");
-  ASSERT_EQ(object.remaining() , 0 );
+  bool guess1 = object.match("Secre"); //remaining =2
+  bool guess2 = object.match("Secre"); //remaining =1
+  bool guess3 = object.match("Secre");//remaining =0
   bool correct_guess = object.match("Secret");
-  ASSERT_FALSE(correct_guess);
+  ASSERT_FALSE(correct_guess); //should be false because your're locked out from no remaining guesses
   
 }
 
@@ -123,32 +122,29 @@ TEST(GuesserTest, locked_from_no_remaining_guesses)
 TEST(GuesserTest, remaining_not_decreased_with_distance_greater_than_2)
 {
    Guesser object("Secret"); 
-  bool guess = object.match("Secretiojfojeofejo");
-  ASSERT_FALSE(guess); 
-  ASSERT_EQ(object.remaining(), 2);
+  bool guess = object.match("Secretiojfojeofejo"); //distance =12
+  ASSERT_EQ(object.remaining(), 2); //should return 2 because guess was incorrect meaning remaing should be decreased
 
 }
 
 TEST(GuesserTest, blank_guess)
 {
   Guesser object(""); 
-  ASSERT_TRUE(object.match("")); 
-  ASSERT_EQ(object.remaining(), 3);
+  bool guess=object.match(""); 
+  ASSERT_EQ(object.remaining(), 3);//remaining is still 3 as correct guess has been made
 }
 
 TEST(GuesserTest, secret_is_truncated_to_32_charecters)
 {
   Guesser object("123456789012345678901234567890123");
-  ASSERT_TRUE(object.match("12345678901234567890123456789012"));
+  ASSERT_TRUE(object.match("12345678901234567890123456789012"));//should be what user inputed not including the last charecter
  
 }
 
 TEST(GuesserTest, remaining_not_reset_to_3_with_correct_guess)
 {
    Guesser object("Secret");
-  bool guess = object.match("Secre");
-  	ASSERT_EQ(object.remaining(), 2);
-   bool correct_guess = object.match("Secret");
- 
-   ASSERT_EQ(object.remaining(), 3);
+  bool guess = object.match("Secre");//remaining =2
+   bool correct_guess = object.match("Secret");//correct guess
+   ASSERT_EQ(object.remaining(), 3);//remaining should be reset to 3 because of correct guess
 }
